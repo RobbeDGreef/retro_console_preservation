b7563709eb62227558ea7680df6aaf047dc14ac8
RobbeDGreef <robbe.de.greef@vub.be>
2023-04-04 22:23:25 +0200

    refactor: renamed time.c, added header and micros func

--- src/c_support/timing.c
+++ src/c_support/timing.c
@@ -14,3 +14,13 @@ void get_time_micro(sail_int *rop, unit u) {
     mpz_mul_ui(*rop, *rop, 1000000);
     mpz_add_ui(*rop, *rop, t.tv_nsec / 1000);
 }
+
+/* I know this is the same function as the sleep_micro sail one but
+ * it doesn't use the bigint and this is just easier.
+ */
+unsigned long micros()
+{
+    struct timespec t;
+    clock_gettime(CLOCK_REALTIME, &t);
+    return t.tv_sec * 1000000 + (t.tv_nsec / 1000);
+}
---
+++ src/c_support/timing.h
@@ -0,0 +1,8 @@
+#ifndef __TIMING_H
+#define __TIMING_H
+
+#include <unistd.h>
+
+unsigned long micros();
+
+#endif /* __TIMING_H */
