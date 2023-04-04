5c69ae1fdf7a478e82ce9efe061e9642db7c0e48
RobbeDGreef <robbe.de.greef@vub.be>
2023-04-05 00:56:36 +0200

    fix: functions did not return a value

--- src/c_support/print.c
+++ src/c_support/print.c
@@ -3,9 +3,11 @@
 unit print_instruction(const sail_int addr, const sail_string assembly)
 {
     printf("%04x\t%s\n", mpz_get_ui(addr), assembly);
+    return UNIT;
 }
 
 unit print_string(sail_string s1, sail_string s2)
 {
     printf("%s %s\n", s1, s2);
+    return UNIT;
 }
