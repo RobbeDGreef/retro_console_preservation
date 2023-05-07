e5ef7a3752afc9e5bd5957440b3a51e8d1a9dfaf
RobbeDGreef <robbe.de.greef@vub.be>
2023-05-07 23:58:52 +0200

    feat: implemented interrut system

---
+++ src/c_support/interrupts.c
@@ -0,0 +1,58 @@
+#include <stdint.h>
+#include <stdio.h>
+#include "memory.h"
+
+extern uint64_t zinterrupt_flag;
+extern uint64_t zinterrupt_master;
+extern uint64_t zinterrupt_enable;
+
+#define IM zinterrupt_master
+#define IF zinterrupt_flag
+#define IE zinterrupt_enable
+
+
+/**
+ * @brief Mark interrupt pending
+ * 
+ * @param interrupt the interrupt to mark
+ */
+void interrupt(int interrupt)
+{
+    IF |= interrupt;
+}
+
+void interrupts_enable()
+{
+    IM = 1;
+}
+
+void interrupts_disable()
+{
+    IM = 0;
+}
+
+void io_write_if(uint64_t addr, int bytes, uint64_t val)
+{
+    IF = val;
+}
+
+uint64_t io_read_if(uint64_t addr, int bytes)
+{
+    return IF;
+}
+
+void interrupt_set_ei(uint64_t val)
+{
+    IE = val;
+}
+
+uint64_t interrupt_get_ei()
+{
+    return IE;
+}
+
+void interrupts_init()
+{
+    register_io_read_handler(0xFF0F, io_read_if);
+    register_io_write_handler(0xFF0F, io_write_if);
+}
---
+++ src/c_support/interrupts.h
@@ -0,0 +1,16 @@
+#ifndef __INTERRUPTS_H
+#define __INTERRUPTS_H
+
+#define INTERRUPT_ENABLE_ADDR 0xFFFF
+
+#define INTERRUPT_TIMER (1 << 2)
+
+void interrupts_enable();
+void interrupt(int interrupt);
+void interrupts_disable();
+void interrupts_init();
+
+void interrupt_set_ei(uint64_t val);
+uint64_t interrupt_get_ei();
+
+#endif /* __INTERRUPTS_H */
