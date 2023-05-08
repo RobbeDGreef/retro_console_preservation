8d1f90abc1b1daa0eace107433a882665d41fcfa
RobbeDGreef <robbe.de.greef@vub.be>
2023-05-08 16:49:12 +0200

    feat: added all init functions inside main

--- src/c_support/main.c
+++ src/c_support/main.c
@@ -1,11 +1,18 @@
 #include <pthread.h>
 #include "screen.h"
 #include "memory.h"
+#include "debug.h"
+#include "timer.h"
+#include "timing.h"
+#include "interrupts.h"
 
 int model_main(int argc, char *argv[]);
 
 int main(int argc, char *argv[])
 {
+    debug_init();
+    interrupts_init();
+    timing_init();
     memory_init();
     timer_init();
     screen_init();
