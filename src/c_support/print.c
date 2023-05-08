a8064f6dc6e24b04ddf909da4d3186031b0fd2f2
RobbeDGreef <robbe.de.greef@vub.be>
2023-05-08 16:45:38 +0200

    feat: printing of cycles is added to print_instruction
    Also added color support for easier reading in terminal.

--- src/c_support/print.c
+++ src/c_support/print.c
@@ -1,8 +1,18 @@
 #include "sail.h"
 
-unit print_instruction(const sail_int addr, const sail_string assembly)
+#define PRINT_CYCLES 1
+
+#define BLUE "\033[36;1m"
+#define END "\033[m"
+
+unit print_instruction(const sail_int addr, const sail_string assembly, sail_int passed_cycles)
 {
-    printf("%04x\t%s\n", mpz_get_ui(addr), assembly);
+    printf("%04x\t" BLUE "%s" END
+#if PRINT_CYCLES
+           "\t[cycle: %lu]"
+#endif
+           "\n",
+           mpz_get_ui(addr), assembly, mpz_get_ui(passed_cycles));
     return UNIT;
 }
 
